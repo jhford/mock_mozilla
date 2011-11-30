1cd412dcf8dcc16880d0a0ffd48301e8fc55750b
John Ford <john@johnford.info>
2011-11-30 13:55:48 -0800

    changes to the proof of concept script

--- poc.sh
+++ poc.sh
@@ -1,20 +1,11 @@
 #!/bin/bash
 
 TARGET="$1"
-BUILDDIR="/builds/$1"
 T_BUILDDIR="/builds"
-H_BUILDDIR="/var/lib/mock_mozilla/$TARGET/root/$T_BUILDDIR"
 
 test -z $TARGET && echo "You need to specify a target" 1>&2 && exit 1
 
-sudo mkdir -p $H_BUILDDIR $BUILDDIR
-sudo chown jhford:jhford $H_BUILDDIR
-
-mock_mozilla -r $TARGET --clean
 mock_mozilla -r $TARGET --init 
-
-sudo mount --bind $BUILDDIR $H_BUILDDIR
-
 mock_mozilla -r $TARGET --install zip unzip autoconf213 gtk2-devel libnotify-devel yasm alsa-lib-devel curl-devel
 
 (cd $BUILDDIR && hg clone http://hg.mozilla.org/mozilla-central)
