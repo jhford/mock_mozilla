8aec5f22adbdeaa853a6c0f7c59cde4a2ded8838
John Ford <john@johnford.info>
2011-12-01 14:14:29 -0800

    updates to poc script

--- poc.sh
+++ poc.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-TARGET="$1"
+TARGET="fedora-15-x86_64-mozilla"
 T_BUILDDIR="/builds"
+BUILDDIR="/var/lib/mock_mozilla/fedora-15-x86_64-mozilla/root/"
+
+MOCK_OPTS="--verbose --unpriv -r $TARGET"
 
 test -z $TARGET && echo "You need to specify a target" 1>&2 && exit 1
 
-mock_mozilla -r $TARGET --init 
-mock_mozilla -r $TARGET --install zip unzip autoconf213 gtk2-devel libnotify-devel yasm alsa-lib-devel curl-devel
+mock_mozilla $MOCK_OPTS --init 
+mock_mozilla $MOCK_OPTS --install zip unzip autoconf213 gtk2-devel libnotify-devel yasm alsa-lib-devel curl-devel
 
 (cd $BUILDDIR && hg clone http://hg.mozilla.org/mozilla-central)
 cat > $BUILDDIR/mozilla-central/.mozconfig << EOF
@@ -14,5 +17,5 @@ mk_add_options MOZ_MAKE_FLAGS="-j4"
 mk_add_options MOZ_OBJDIR="../objdir"
 EOF
 
-mock_mozilla -r $TARGET --shell "/bin/bash -c \"make -f client.mk build\"" --cwd="$T_BUILDDIR"
-mock_mozilla -r $TARGET --shell "/bin/bash -c \"make package\"" --cwd="$T_BUILDDIR/objdir"
+mock_mozilla $MOCK_OPTS --cwd mozilla-central --shell make -f client.mk build
+mock_mozilla $MOCK_OPTS --cwd objdir --shell make package
